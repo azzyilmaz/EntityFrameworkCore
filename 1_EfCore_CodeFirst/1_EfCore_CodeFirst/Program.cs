6726d991f85ffe96bb1f29c9e01af3ad322f6eee
Aziz Yilmaz <azzylmz93@gmail.com>
2022-12-07 20:44:16 +0300

    Add project files.

---
+++ 1_EfCore_CodeFirst/1_EfCore_CodeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+
+public class EticaretContext : DbContext 
+{ 
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=AZZYLMZ\\SQLEXPRESS;Database=ETicaretDB;Trusted_Connection=True;");
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+}
